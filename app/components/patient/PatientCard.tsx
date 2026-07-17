e83660948e5a4c498f366b6ee848476bf88d1989
Priyanshu Pandey <pandeypriyanshu7777@gmail.com>
2026-07-18 01:06:56 +0530

    feat: render patient data dynamically using reusable components

--- app/components/patient/PatientCard.tsx
+++ app/components/patient/PatientCard.tsx
@@ -1,3 +1,20 @@
-export default function PatientCard(){
-    
+import type { Patient } from "../types/patient";
+
+type PatientCardProps = {
+  patient: Patient;
+};
+
+export default function PatientCard({ patient }: PatientCardProps) {
+  return (
+    <div>
+      {Object.entries(patient).map(([key, value]) => (
+        <p key={key}>
+          <div>
+            <strong>{key}:</strong>
+            {value}
+          </div>
+        </p>
+      ))}
+    </div>
+  );
 }
